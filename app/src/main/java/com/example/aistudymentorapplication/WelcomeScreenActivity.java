ec0ac8a8e4107ba542e0e23d2dafeaa0755ced18
Trần Phương Uyên <tranphuonguyen.judy@gmail.com>
2026-07-20 20:44:30 +0700

    Music

--- app/src/main/java/com/example/aistudymentorapplication/WelcomeScreenActivity.java
+++ app/src/main/java/com/example/aistudymentorapplication/WelcomeScreenActivity.java
@@ -1,6 +1,7 @@
 package com.example.aistudymentorapplication;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,11 +9,15 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
-
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.background_music);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
 
         Button btnGetStarted = findViewById(R.id.btnGetStarted);
         btnGetStarted.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +28,15 @@ public class WelcomeScreenActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
